3bf5bf92257342ff9830ac3a29b5f90e70983601
Nevil-ing <nevilpurpp12@gmail.com>
2025-11-14 12:57:02 +0300

    added github actions

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import requests
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+def get_ip_address():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        response.raise_for_status()
+        ip_data = response.json()
+        return ip_data.get('ip', 'IP address not found')
+    except requests.RequestException as e:
+        return f"Error occurred: {e}"
+
+@app.get("/ip")
+async def get_client_ip(request: Request):
+    client_host = request.client.host
+    return JSONResponse(content={"ipnew": get_ip_address()})
